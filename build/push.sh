7f40622dd9f78df9157e5172907f20366eaaf1b6
Dale R. Hille <dalehill@us.ibm.com>
2020-02-04 06:04:14 -0500

    tag update

--- build/push.sh
+++ build/push.sh
@@ -1,11 +1,11 @@
-docker push "us.icr.io/dalehille/webapp:${TRAVIS_TAG}" 
+docker push "us.icr.io/dalehille/webapp:${VERSION}" 
 
 curl --request POST \
        --url "${RAZEE_API}/api/v1/channels/${RAZEE_CHANNEL}/version" \
        --header "content-type: text/yaml" \
        --header "razee-org-key: ${RAZEE_ORG_KEY}" \
        --header "resource-description: ${TRAVIS_COMMIT_MESSAGE}" \
-       --header "resource-name: ${TRAVIS_TAG}" \
+       --header "resource-name: ${VERSION}" \
        --header "x-api-key: ${X_API_KEY}" \
        --header "x-user-id: ${X_USER_ID}" \
        --data-binary @/tmp/resource.yaml
