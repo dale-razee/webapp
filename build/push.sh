8f4737bb0cde28ba2c1c3bc53b34cd54fc8c6528
Dale R. Hille <dalehill@us.ibm.com>
2020-06-09 10:10:30 -0400

    Update push.sh

--- build/push.sh
+++ build/push.sh
@@ -6,13 +6,33 @@ cat /tmp/resource.yaml
       #  --header "resource-description: ${TRAVIS_COMMIT_MESSAGE}" \
 echo 
 set -x 
+# curl --verbose \
+#        --url "${RAZEE_API}/api/v1/channels/${RAZEE_CHANNEL}/version" \
+#        --header "content-type: text/yaml" \
+#        --header "razee-org-key: ${RAZEE_ORG_KEY}" \
+#        --header "resource-name: ${VERSION}" \
+#        --header "x-api-key: ${X_API_KEY}" \
+#        --header "x-user-id: ${X_USER_ID}" \
+#        --data-binary @/tmp/resource.yaml
+
 curl --verbose \
-       --url "${RAZEE_API}/api/v1/channels/${RAZEE_CHANNEL}/version" \
-       --header "content-type: text/yaml" \
-       --header "razee-org-key: ${RAZEE_ORG_KEY}" \
-       --header "resource-name: ${VERSION}" \
-       --header "x-api-key: ${X_API_KEY}" \
-       --header "x-user-id: ${X_USER_ID}" \
-       --data-binary @/tmp/resource.yaml
+     --url "${RAZEE_API}/graphql" \
+     --header "content-type: multipart/form-data" \
+     --header "x-api-key: ${X_API_KEY}" \
+     --form 'operations={
+    "query": "mutation addChannelVersion($org_id:String!, $channel_uuid:String!, $name:String!, $type:String!, $file:Upload!, $description:String) {\n  addChannelVersion(org_id:$org_id,channel_uuid:$channel_uuid, name:$name, type:$type, file:$file, description:$description){\n    success\n    version_uuid\n  }\n}",
+    "variables": {
+      "org_id": ${ORG_ID},
+      "channel_uuid": ${CHANNEL_ID},
+      "name": ${VERSION},
+      "type": "application/yaml",
+      "file": null,
+      "description": null
+    },
+    "operationName": "addChannelVersion"
+  }' \
+    --form 'map={"localfile":["variables.file"]}' \
+    --form localfile=@resource.yaml
+
 
 echo $?
