75c71a95039cc82f60b46b8e14282e3155e04f4b
Dale R. Hille <dalehill@us.ibm.com>
2020-06-10 16:27:24 -0400

    push to razee

--- build/push.sh
+++ build/push.sh
@@ -1,19 +1,8 @@
-
-docker push "us.icr.io/dalehille/webapp:${VERSION}" 
-
+#/bin/sh
 cat /tmp/resource.yaml
 
-      #  --header "resource-description: ${TRAVIS_COMMIT_MESSAGE}" \
 echo 
 set -x 
-# curl --verbose \
-#        --url "${RAZEE_API}/api/v1/channels/${RAZEE_CHANNEL}/version" \
-#        --header "content-type: text/yaml" \
-#        --header "razee-org-key: ${RAZEE_ORG_KEY}" \
-#        --header "resource-name: ${VERSION}" \
-#        --header "x-api-key: ${X_API_KEY}" \
-#        --header "x-user-id: ${X_USER_ID}" \
-#        --data-binary @/tmp/resource.yaml
 
 curl --verbose \
      --url "${RAZEE_API}/graphql" \
